760eb782323743bfcbbfc9ba0991631a03293e79
Bart <mail@bartbrouwers.com>
2020-02-28 10:32:08 +0100

    Reservering Model afgemaakt.

--- Slijterij Sjonnie/Models/Reservering.cs
+++ Slijterij Sjonnie/Models/Reservering.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace Slijterij_Sjonnie.Models
 {
     public class Reservering
     {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public Whisky Whisky { get; set; }
+        public int Aantal { get; set; }
+        public DateTime Datum { get; set; }
     }
 }
